ca65924c0e7af771d45a1e542f21dcf77a03107a
toto9602 <toto9602@naver.com>
2025-02-19 22:40:45 +0900

    pm2 설정 파일 수정, crypto 모듈 추가

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  apps: [
-    {
-      name: "ffiv",
-      script: "./dist/main.js",
-      instances: 1,
-      exec_mode: "cluster",
-      log_file: "./logs/ffiv-combined.log",
-      out_file: "./logs/ffiv-out.log",
-      error_file: "./logs/ffiv-error.log",
-      log_date_format: "YYYY-MM-DD HH:mm:ss",
-      merge_logs: true,
-    },
-  ],
+  name: "ffiv",
+  script: "bun",
+  args: "nest start",
+  instances: 1,
+  exec_mode: "fork",
+  log_file: "./logs/ffiv-combined.log",
+  out_file: "./logs/ffiv-out.log",
+  error_file: "./logs/ffiv-error.log",
+  log_date_format: "YYYY-MM-DD HH:mm:ss",
+  merge_logs: true,
+  env: {
+    NODE_ENV: "production",
+  },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import crypto from "crypto";
+
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
