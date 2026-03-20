a3077e2fa1bbdeddfec22962b52f792bf7eb0636
toto9602 <toto9602@naver.com>
2026-03-21 00:05:02 +0900

    replyMessageId 삭제

--- src/runner/entity/message.reply.history.entity.ts
+++ src/runner/entity/message.reply.history.entity.ts
@@ -12,20 +12,15 @@ export class MessageReplyHistory extends BaseEntity {
   @Property({ type: "varchar", name: "original_message_id" })
   originalMessageId: string;
 
-  @Property({ type: "varchar", name: "reply_message_id" })
-  replyMessageId: string;
-
   public static of(args: {
     sentAt: Date;
     eventId: number;
     originalMessageId: string;
-    replyMessageId: string;
   }): MessageReplyHistory {
     const entity = new MessageReplyHistory();
     entity.sentAt = args.sentAt;
     entity.eventId = args.eventId;
     entity.originalMessageId = args.originalMessageId;
-    entity.replyMessageId = args.replyMessageId;
     return entity;
   }
 }
